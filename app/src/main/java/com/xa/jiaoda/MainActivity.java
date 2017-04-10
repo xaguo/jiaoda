5969fdc4e4394b13569d85d2393d09dcf24aa868
xaguo <xa_guo@163.com>
2017-04-10 19:27:09 +0800

    First commit.

---
+++ app/src/main/java/com/xa/jiaoda/MainActivity.java
@@ -0,0 +1,36 @@
+package com.xa.jiaoda;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.AlphaAnimation;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       // initAnimation();
+        enterHome();
+
+    }
+    /**
+     * 进入主界面函数
+     */
+    private void enterHome() {
+        Intent intent = new Intent(this, ShortRoadActivity.class);
+        startActivity(intent);
+        finish();
+    }
+    /**
+     * 添加淡入动画效果
+     */
+    private void initAnimation() {
+        AlphaAnimation alphaAnimation = new AlphaAnimation(1, 0);
+        alphaAnimation.setDuration(3000);
+        findViewById(R.id.activity_main).startAnimation(alphaAnimation);
+        enterHome();
+    }
+}
